c037fba5638ea616030ff7212bc4acaf0f44f119
Zheng Ziming <zhengziming@zhizhangyi.com>
2019-10-15 16:31:37 +0800

    add zm-qrcode.aar

--- app/src/main/java/com/example/zheng/qrcode/MainActivity.java
+++ app/src/main/java/com/example/zheng/qrcode/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.zheng.qrcode;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.zxing.client.android.CaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, CaptureActivity.class));
+        finish();
     }
 }
